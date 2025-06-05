f759e366b8c3ef58764bd37280a951ec07497768
MohammedMahi1 <mohammed.mahi012@gmail.com>
2025-06-05 18:34:09 +0100

    update: ui/container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,46 +9,48 @@ import { twMerge } from "tailwind-merge";
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false)
 
-  const [xb, setXb] = useState()
-  const [yb, setYb] = useState()
+  const [xb, setXb] = useState(null)
+  const [yb, setYb] = useState(null)
   const inputRef: any = useRef(null);
 
   useEffect(() => {
-    setXb(inputRef.current.offsetLeft)
-    setXb(inputRef.current.offsetLeft)
+    setXb(inputRef.current.offsetLeft.toString())
+    setYb(inputRef.current.offsetTop.toString())
   }, []);
-  console.log(xb);
+  console.log(typeof xb);
 
   return (
+    <div>
+
     <Container>
       <h1>Hello Next</h1>
-      <Button className="" onClick={() => setIsOpen(!isOpen)}
+      <Button onClick={() => setIsOpen(!isOpen)}
         ref={inputRef}
-      >
+        >
         Dark
       </Button>
-      <div className={`absolute top-${yb} left-${xb} overflow-hidden w-[100vw] h-[100vh] z-[-1] flex items-center justify-center `}>
-        <motion.div
-          className={twMerge(` bg-black rounded-[9999999px] flex items-center justify-center`)}
-          initial={{
+
+    </Container >
+          <motion.div
+        className={twMerge(`absolute top-[${yb}px] left-[${xb}px] bg-black rounded-[9999999px] flex items-center justify-center`,xb === undefined  || yb === undefined ?  "hidden":"")}
+        initial={{
+          width: 10,
+          height: 10,
+          top: yb + "px",
+          left: xb + "px"
+        }}
+        animate={
+          isOpen ? {
+            width: "3000px",
+            height: "3000px",
+          } : {
             width: 10,
             height: 10,
-            top: yb,
-            left: xb
-          }}
-          animate={
-            isOpen ? {
-              width: "3000px",
-              height: "3000px",
-            } : {
-              width: 10,
-              height: 10,
-            }
           }
-          transition={{ duration: .3 }}
-        >
-        </motion.div>
-      </div>
-    </Container>
+        }
+        transition={{ duration: .3 }}
+      >
+      </motion.div>
+        </div>
   );
 }
--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -11,7 +11,7 @@ const Container = ({ children,className, ...props }: ContainerProps) => {
     
     
     return (
-        <div className={twMerge('font-semibold max-sm:px-6 max-md:px-20 max-lg:px-60 min-lg:px-100 transition-[3s] flex flex-col items-center justify-center h-svh',className)} {...props}>
+        <div className={twMerge('font-semibold max-sm:px-6 max-md:px-20 max-lg:px-60 min-lg:px-100 transition-[3s] flex flex-col items-end justify-start h-svh',className)} {...props}>
             {children}
         </div>
     )
